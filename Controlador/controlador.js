9b2144c77403ace83513f7dab680e1cfff19017a
Laura Ortiz <153041507+kf-bxgl1@users.noreply.github.com>
2024-03-11 10:10:22 -0500

    Pantalla Editar inventario, Eliminar Inventario, Crear Entrada y Salida, Navegacion
    Se terminan pantallas de edicion de inventario, eliminar inventario, crear entrada y salida.
    Se comienza pantalla de movimientos.
    
    Se continua navegacion entre pantallas, añadiendo navegacion hacia atras, pendiente revision para devolverse varias veces.

--- Controlador/controlador.js
+++ Controlador/controlador.js
@@ -4,30 +4,35 @@
 
 let vista = new Vista();
 
-window.onload = function(){
-    vista.mostrar_plantilla("pagina_inicio", "contenedor_principal");
+
+// window.onload = function(){
+//      vista.mostrar_plantilla("pagina_inicio", "contenedor_principal", 1);
+// }
+
+function regresar_pantalla(){
+    vista.regresar_pantalla();
 }
 
 function mostrar_form_registro_usuario(){
-    vista.mostrar_plantilla("registro_usuario", "contenedor_principal");
+    vista.mostrar_plantilla("registro_usuario", "contenedor_principal", 1);
     vista.mostrar_plantilla("nav_inf_reg","navegador_inf")
 }
 
 function mostrar_form_login(){
-    vista.mostrar_plantilla("log_in", "contenedor_principal");
+    vista.mostrar_plantilla("log_in", "contenedor_principal", 1);
     vista.mostrar_plantilla("nav_inf_login","navegador_inf")
 }
 
 function mostrar_inv_vacia(){
-    vista.mostrar_plantilla("inventarios_vacia", "contenedor_principal");
+    vista.mostrar_plantilla("inventarios_vacia", "contenedor_principal", 1);
     vista.mostrar_plantilla("nav_sup_menu_ham","navegador_sup")
     vista.mostrar_plantilla("nav_inf_con_btns","navegador_inf")
 }
 
 function mostrar_form_crear_inv(){
-    vista.mostrar_plantilla("crear_inventario", "contenedor_principal");
+    vista.mostrar_plantilla("crear_inventario", "contenedor_principal", 1);
 }
 
 function mostrar_inventarios(){
-    vista.mostrar_plantilla("inventarios", "contenedor_principal");
+    vista.mostrar_plantilla("inventarios", "contenedor_principal", 1);
 }
--- Vista/js/vista.js
+++ Vista/js/vista.js
@@ -1,5 +1,8 @@
 class Vista {
-  constructor() {}
+  constructor() {
+    this.stack_pantallas = []; //Contiene el historial de navegacion de pantallas
+
+  }
 
   /**
    * Metodo para desplegar una plantilla en un contenedor
@@ -8,7 +11,7 @@ class Vista {
    * @returns none
    */
 
-  mostrar_plantilla(plantilla, contenedor) {
+  mostrar_plantilla(plantilla, contenedor, pila=null) {
     let cont = document.getElementById(contenedor);
     cont.innerHTML = "";
     //clonar el contenido del template
@@ -17,5 +20,21 @@ class Vista {
         let clone = template.content.cloneNode(true);
         cont.appendChild(clone);
     }
+    if(pila){
+        this.stack_pantallas.push(plantilla);
+    }   
   }
+
+  regresar_pantalla() {
+    let plantilla = this.stack_pantallas.pop(); //Toma el nombre de la pantalla anterior
+    if (this.stack_pantallas.length > 0) {
+      let cont = document.getElementById("contenedor_principal");
+      cont.innerHTML = "";
+      plantilla=this.stack_pantallas.pop();
+      let template = document.getElementById(plantilla);
+      let clone = template.content.cloneNode(true);
+      cont.appendChild(clone);
+    }
+  }
+
 }
