790090ed96455c9fe031d0334068dc53bef508c2
Laura Ortiz <153041507+kf-bxgl1@users.noreply.github.com>
2024-03-16 19:42:54 -0500

    Pantallas de estadisticas y menu lateral, Navegación
    Se hacen pantallas para la selección de informe y sus respectivos filtros, queda pendiente pantallas con el grafico al igual que pantallas de eliminación.
    
    Se añaden notas para mejor entendimiento del código.
    
    Se continua con la navegación entre pantallas, se realiza por secciones de la pagina.
    
    Queda pendiente navegación del nav inf, al igual que corrección de método de la clase Vista para regresar entre pantallas.

--- Controlador/controlador.js
+++ Controlador/controlador.js
@@ -13,6 +13,8 @@ function regresar_pantalla(){
     vista.regresar_pantalla();
 }
 
+// Funciones para cambiar plantillas desde la pantalla Inicio
+
 function mostrar_form_registro_usuario(){
     vista.mostrar_plantilla("registro_usuario", "contenedor_principal", 1);
     vista.mostrar_plantilla("nav_inf_reg","navegador_inf")
@@ -23,16 +25,72 @@ function mostrar_form_login(){
     vista.mostrar_plantilla("nav_inf_login","navegador_inf")
 }
 
+// Funcion para cambiar plantillas desde la pantalla de Registro de Usuario y Log In
+
 function mostrar_inv_vacia(){
     vista.mostrar_plantilla("inventarios_vacia", "contenedor_principal", 1);
     vista.mostrar_plantilla("nav_sup_menu_ham","navegador_sup")
     vista.mostrar_plantilla("nav_inf_con_btns","navegador_inf")
 }
 
+// Funciones para cambiar plantillas desde la pantalla de Inventario
+
+function mostrar_inventarios(){
+    vista.mostrar_plantilla("inventarios", "contenedor_principal", 1);
+}
+
 function mostrar_form_crear_inv(){
     vista.mostrar_plantilla("crear_inventario", "contenedor_principal", 1);
 }
 
-function mostrar_inventarios(){
-    vista.mostrar_plantilla("inventarios", "contenedor_principal", 1);
+function mostrar_editar_inv(){
+    vista.mostrar_plantilla("editar_inventario", "contenedor_principal", 1);
+}
+
+// Funciones para cambiar plantillas desde la pantalla de Movimientos
+
+function mostrar_form_crear_entrada(){
+    vista.mostrar_plantilla("crear_entrada", "contenedor_principal", 1);
+}
+
+function mostrar_form_crear_salida(){
+    vista.mostrar_plantilla("crear_salida", "contenedor_principal", 1);
+}
+
+// Funciones para cambiar plantillas desde la pantalla de Stock
+
+function mostrar_detalles_producto(){
+    vista.mostrar_plantilla("detalles_producto", "contenedor_principal", 1);
+}
+
+function mostar_form_crear_producto(){
+    vista.mostrar_plantilla("crear_producto", "contenedor_principal", 1);
+}
+
+function mostrar_categorias(){
+    vista.mostrar_plantilla("categorias", "contenedor_principal", 1);
+}
+
+function mostrar_abastecimiento(){
+    vista.mostrar_plantilla("abastecimiento", "contenedor_principal", 1);
+}
+
+// Funciones para cambiar plantillas desde la pantalla de Detalles Producto
+
+function mostrar_mov_del_producto(){
+    vista.mostrar_plantilla("movimientos_del_producto", "contenedor_principal", 1);
+}
+
+function mostrar_editar_producto(){
+    vista.mostrar_plantilla("editar_producto", "contenedor_principal", 1);
+}
+
+// Funciones para cambiar plantillas desde la pantalla de Categorias
+
+function mostrar_editar_categoria(){
+    vista.mostrar_plantilla("editar_categoria", "contenedor_principal", 1);
+}
+
+function mostrar_form_crear_categoria(){
+    vista.mostrar_plantilla("crear_categoria", "contenedor_principal", 1);
 }
